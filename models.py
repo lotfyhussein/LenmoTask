06f09a5d23adfe45db36bf0e6784f487efcf9a62
Lotfy <lotfyhussein@aucegypt.edu>
2019-09-05 13:29:06 +0200

    Feat: Add Payment Payback

--- models.py
+++ models.py
@@ -1,20 +1,20 @@
 import uuid
 class User:
-    def __init__(self, type, username, balance):
-        self.type = type
-        self.username = username
-       	self.balance = balance
+    def __init__(self, Type, Username, Balance):
+        self.Type = Type
+        self.Username = Username
+       	self.Balance = Balance
         self.LoanRequests = []
         self.Offers = []
 
-    def add_Loan_Request(self, LoanRequest):
-    	if self.type == 'Borrower':
+    def Add_Loan_Request(self, LoanRequest):
+    	if self.Type == 'Borrower':
     		self.LoanRequests.append(LoanRequest)
     	else:
     		raise Exception('an Investor Cannot perform this request')
-    def add_Offer(self, Offer):
-    	if self.type == 'Investor':
-    		if self.balance >= Offer.amount + Offer.Lenmo_Fees:
+    def Add_Offer(self, Offer):
+    	if self.Type == 'Investor':
+    		if self.Balance >= Offer.Amount + Offer.Lenmo_Fees:
     			self.Offers.append(Offer)
     		else:   
     			raise Exception('Insufficient Funds')
@@ -22,78 +22,90 @@ class User:
     		raise Exception('a Borrower Cannot perform this request')
 
 
-    def get_User_Type(self):
-    	return self.type
+    def Get_User_Type(self):
+    	return self.Type
 
-    def get_UserName(self):
-   		return str(self.username)
+    def Get_UserName(self):
+   		return str(self.Username)
 
-    def get_User_Loans(self):
-    	if self.type == 'Borrower':
+    def Get_User_Loans(self):
+    	if self.Type == 'Borrower':
     		return self.LoanRequests
     	else:
     		return self.Offers
 
 class LoanRequest:
-    def __init__(self, Borrower, amount, period):
+    def __init__(self, Borrower, Amount, Period):
         self.ID = uuid.uuid4()
         self.Borrower = Borrower	
-        self.amount = amount
-        self.period = period	
+        self.Amount = Amount
+        self.Period = Period	
     def getAmount(self):
-    	return self.amount
+    	return self.Amount
     def getPeriod(self):
-    	return self.period
+    	return self.Period
     def __str__(self):
-    	return "Loan_Request_ID: {},\nBorrower: {},\namount: {},\nperiod: {}\n".format(self.ID, self.Borrower, self.amount,self.period)
-
+    	return "Loan_Request_ID: {},\nBorrower: {},\nAmount: {},\nPeriod: {}\n".format(self.ID, self.Borrower, self.Amount,self.Period)
 
 
 class Offer:
-    def __init__(self, Loan_Request_ID, Investor, amount, period, interest):
+    def __init__(self, Loan_Request_ID, Investor, Amount, Period, Interest):
         self.ID = uuid.uuid4()
         self.Loan_Request_ID = Loan_Request_ID
         self.Investor = Investor	
-        self.amount = amount
-        self.period = period		
-        self.interest = interest
+        self.Amount = Amount
+        self.Period = Period		
+        self.Interest = Interest
         self.Lenmo_Fees = 3
     def get_Amount(self):
-    	return self.amount
+    	return self.Amount
     def get_Period(self):
-    	return self.period
+    	return self.Period
     def get_Interest(self):
-    	return self.interest
+    	return self.Interest
     def get_Investor(self):
     	return self.Investor
     def __str__(self):
-    	return"Offer_ID: {},\nLoan_Request_ID: {},\nInvestor: {},\namount: {},\nperiod: {},\nInterest: {}\n".format(self.ID, self.Loan_Request_ID, self.Investor, self.amount,self.period, self.interest)
-
-
-
-
-# class Loan:
-#     def __init__(self,Investor, amount, period, interest, Borrower, status):
-#     def get_Status(self):
-#     	return self.status
-#     def print_Details(self):
-#     	print("Investor: {}, Borrower: {} , amount: {}, period: {}, Interest: {}, Status: {}".format(self.Investor, self.Borrower, self.amount,self.period, self.interest, self.status))
-
+    	return"Offer_ID: {},\nLoan_Request_ID: {},\nInvestor: {},\nAmount: {},\nPeriod: {},\nInterest: {}\n".format(self.ID, self.Loan_Request_ID, self.Investor, self.Amount,self.Period, self.Interest)
 
 class Loan:
-    def __init__(self,Loan_Request, Offer, status):
+    def __init__(self,Loan_Request, Offer, Status):
         self.ID = uuid.uuid4()
         self.Loan_Request_ID = Loan_Request.ID
+        self.Borrower = Loan_Request.Borrower
         self.Offer_ID = Offer.ID
         self.Investor = Offer.Investor	
-        self.amount = Offer.amount
-        self.period = Offer.period		
-        self.interest = Offer.interest
-        self.status = status
-    def get_Status(self):
-    	return self.status
+        self.Amount = Offer.Amount
+        self.Period = Offer.Period		
+        self.Interest = Offer.Interest 
+        self.Status = Status
+    def Schedule_Loan_Payments(self):
+        Loan_Payments = []
+        Todays_Date = 1 #Assume it's January
+        for i in range (self.Period):
+            Loan_Payments.append(LoanPayment(self, Todays_Date + 1))
+            Todays_Date+=1
+            
+        return Loan_Payments
+    def Get_Status(self):
+    	return self.Status
     def __str__(self):
-    	return"Offer_ID: {},\nLoan_Request_ID: {},\nInvestor: {},\namount: {},\nperiod: {},\nInterest: {},\nStatus: {}\n".format(self.ID, self.Loan_Request_ID, self.Investor, self.amount,self.period, self.interest, self.status)
+    	return"Loan_ID: {},\nOffer_ID: {},\nLoan_Request_ID: {},\nInvestor: {},\nBorrower: {},\nAmount: {},\nPeriod: {},\nInterest: {},\nStatus: {}\n".format(self.ID,self.Offer_ID, self.Loan_Request_ID, self.Investor, self.Borrower, self.Amount,self.Period, self.Interest, self.Status)
+
+
+class LoanPayment:
+    def __init__(self,Loan, Due_Date):
+        self.ID = uuid.uuid4()
+        self.Loan = Loan
+        self.Due_Date = Due_Date
+    def Get_Due_Date(self):
+        return self.Due_Date
+    def __str__(self):
+        return"Payment_ID: {},\nLoan_ID: {},\nInvestor: {},\nBorrower: {},\nDue Date: {}\n".format(self.ID, self.Loan.ID, self.Loan.Investor, self.Loan.Borrower, self.Due_Date)
+
+
+
+
 
 
 
--- use_case.py
+++ use_case.py
@@ -1,25 +1,39 @@
 from models import *
+import time
 
-Borrower = User('Borrower', 'lotfy', 0)
-Investor =	User('Investor', 'Amr', 20000)
+Borrower = User('Borrower', 'Lotfy', 0)
+Investor =	User('Investor', 'Ahmed', 20000)
 
-Loan_Request = LoanRequest(Borrower.get_UserName(), 5000, 6)
-Offer = Offer(Loan_Request.ID, Investor.get_UserName(), 5000, 6, 15)
+Loan_Request = LoanRequest(Borrower.Get_UserName(), 5000, 6)
+Offer = Offer(Loan_Request.ID, Investor.Get_UserName(), 5000, 6, 15)
 
 try:
-	Borrower.add_Loan_Request(Loan_Request)
+	Borrower.Add_Loan_Request(Loan_Request)
 	print ('Loan Request with below details was submitted Succefully\n')
 	print(Loan_Request)
+	print('---------------')
 except Exception as err:
 	print (err)
-
+time.sleep(3)
 try: 
-	Investor.add_Offer(Offer)
+	Investor.Add_Offer(Offer)
 	print ('Loan Offer with below details was submitted Succefully\n')
 	print(Offer)
+	print('---------------')
 except Exception as err:
 	print (err)
-
+time.sleep(3)   
 print ('Loan with below details was added to the system:\n')
 Loan = Loan(Loan_Request,Offer,'Funded')
 print(Loan)
+print('---------------')
+time.sleep(3)   
+
+
+Loan_Payments = Loan.Schedule_Loan_Payments()
+
+for i in range (len(Loan_Payments)):
+	time.sleep(3)
+	print('Payment with below details was paid:\n')   
+	print (Loan_Payments[i])
+	print('---------------')
